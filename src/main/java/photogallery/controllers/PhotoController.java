56f21fc1de57b2dfae62e8d978c9d898d9faa9ff
cashlegendz <ekoshmarik@gmail.com>
2016-05-22 21:03:24 +0300

    modified project structure & add some files

--- src/main/java/photogallery/controllers/PhotoController.java
+++ src/main/java/photogallery/controllers/PhotoController.java
@@ -1,14 +1,10 @@
-package photogallery;
+package photogallery.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Created by HiMan on 22.05.2016.
- */
 @Controller
 public class PhotoController {
 

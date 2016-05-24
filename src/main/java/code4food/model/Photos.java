ccf17b2ea914b1405804891a30cd3d0a02100cc1
domvit <domvit78@gmail.com>
2016-05-25 02:27:03 +0300

    adding a singleton in which data is stored is loaded gallery

---
+++ src/main/java/code4food/model/Photos.java
@@ -0,0 +1,29 @@
+package code4food.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by HiMan on 25.05.2016.
+ */
+public class Photos {
+
+    private List<Photo> photos = new ArrayList<>();
+
+    private static Photos ourInstance = new Photos();
+
+    public static Photos getInstance() {
+        return ourInstance;
+    }
+
+    private Photos() {
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void addPhoto(Photo photo){
+        photos.add(photo);
+    }
+}

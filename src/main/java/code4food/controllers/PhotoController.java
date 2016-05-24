c0f875e70d5484226edff13a3d8f3a7047720d5e
domvit <domvit78@gmail.com>
2016-05-25 02:25:19 +0300

    changes in the controllers

--- src/main/java/code4food/controllers/PhotoController.java
+++ src/main/java/code4food/controllers/PhotoController.java
@@ -1,21 +1,55 @@
 package code4food.controllers;
 
+import code4food.service.PhotoGalleryServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 @Controller
-@RequestMapping("/photo")
 public class PhotoController {
 
-    @RequestMapping("/photo")
-    public String photo(String name, Model model) {
-        name = "PhotoGallery";
-        model.addAttribute("name", name);
-        //File dir1 = new File("g:\\1");
-        //File dir2 = new File("g:\\2");
-//        проверочка сканфолдера
-        //scanFolders(dir1,dir2);
+//    @RequestMapping("/photo")
+//    public String photo(Model model) {
+//        return "photo";
+//    }
+    private static final String DISPLAY = "display";
+    private static final String NO_DISPLAY = "nodisplay";
+
+//     добавить в метод проверку на наличие загруженной галлереи и в
+//     соответствии с результатом выставлять дисплей/нодисплей
+
+    @RequestMapping(value="/photo", method=RequestMethod.GET)
+    public String photo(Model model) {
+        model.addAttribute("displayActionForm", DISPLAY);
+        model.addAttribute("displayPhoto", NO_DISPLAY);
+        return "photo";
+    }
+
+//    Дописать добавление фотографий (Photo) в лист (Photos)
+    @RequestMapping(value="/photo", method=RequestMethod.POST)
+    public String photoSubmit(@RequestParam("pathName") String pathName, Model model) {
+        String error = "";
+        File destFolder = new File(pathName);
+        File targetFolder;
+        try {
+            targetFolder = File.createTempFile("temp_gallery_folder","");
+            targetFolder.delete();
+            targetFolder.mkdir();
+            new PhotoGalleryServiceImpl().scanFolders(destFolder,targetFolder);
+        } catch (IOException e) {
+            error = e.toString();
+        }
+        model.addAttribute("name",error);
+        model.addAttribute("displayActionForm", NO_DISPLAY);
+        model.addAttribute("displayPhoto", DISPLAY);
         return "photo";
     }
 }

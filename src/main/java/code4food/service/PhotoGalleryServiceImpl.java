5e0ea307b11708855d6f0255caa16a6b060ebe0f
domvit <domvit78@gmail.com>
2016-05-26 01:44:39 +0300

    remove old code

--- src/main/java/code4food/service/PhotoGalleryServiceImpl.java
+++ src/main/java/code4food/service/PhotoGalleryServiceImpl.java
@@ -8,26 +8,6 @@ import java.io.File;
 //import java.nio.file.Files;
 
 public class PhotoGalleryServiceImpl implements PhotoGalleryService {
-
-//  public void scanFolders(File scanfolder, File destinationFolder) {
-//    File[] files = scanfolder.listFiles();
-//    for(File file:files){
-//      if (file.isDirectory()){
-//        scanFolders(file, destinationFolder);
-//      }else {
-//        if (file.getName().toLowerCase().endsWith(".png")) {
-//          String filePath = file.getPath();
-////          System.out.println(filePath + "   " + destinationFolder.getPath()+"\\"+file.getName());
-//          try {
-//            Files.copy(file.toPath(), new File(destinationFolder.getPath() + "\\" + file.getName()).toPath());
-//          } catch (IOException e) {
-//            e.printStackTrace();
-//          }
-//        }
-//      }
-//    }
-//  }
-
   public void scanFolders(File scanfolder) {
     File[] files = scanfolder.listFiles();
     for(File file:files){
